8bd96f29653e357f80ef837b68e36fcb8b728f87
scriptMang <aperalta686@gmail.com>
2026-03-03 01:49:54 -0500

    Clean up the profile_card component html to make it easier to follow

--- src/components/UserProfileCard/UserProfileCard.tsx
+++ src/components/UserProfileCard/UserProfileCard.tsx
@@ -3,14 +3,12 @@ import type { UserProfileCardProps } from '../../types'
 function UserProfileCard({user, showEmail,showRole, onEdit,children}: UserProfileCardProps) {
     return (
         <>
-         <h1>Title</h1>
-         <p>{user.id}</p>
          <p>{user.name}</p>
          {showEmail ? <p>{user.email}</p> :  showEmail}
          {showRole ? <p>{user.role}</p> :  showRole}
          <img src={user.avatarUrl}/>
          <button onClick={() => onEdit ? onEdit(user.id) : null} 
-          style={{backgroundColor: "blue", color: "white"}}>edit</button>
+          style={{backgroundColor: "blue", color: "white"}}>Edit Profile</button>
         {children}
         </>
     )
