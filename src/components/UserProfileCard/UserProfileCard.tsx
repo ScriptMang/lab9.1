f29963acb040fbef0e4065a70db8a562ecaa849e
scriptMang <aperalta686@gmail.com>
2026-03-04 00:14:07 -0500

    Reorganize profilecard elems into divs with id meant to be styled

--- src/components/UserProfileCard/UserProfileCard.tsx
+++ src/components/UserProfileCard/UserProfileCard.tsx
@@ -2,15 +2,20 @@ import type { UserProfileCardProps } from '../../types'
 
 function UserProfileCard({user, showEmail,showRole, onEdit,children}: UserProfileCardProps) {
     return (
-        <>
-         <p>{user.name}</p>
-         {showEmail ? <p>{user.email}</p> :  showEmail}
-         {showRole ? <p>{user.role}</p> :  showRole}
-         <img src={user.avatarUrl}/>
+        <div id="profileCard">
+         <div id= "profileContainer">
+            <img id="userImg" src={user.avatarUrl}/>
+            <div id="profileInfo">
+                <div>{user.name}</div>
+                {showEmail ? <p>{user.email}</p> :  showEmail}
+                {showRole ? <p>{user.role}</p> :  showRole}
+            </div>
+         </div>
+         
          <button onClick={() => onEdit ? onEdit(user.id) : null} 
           style={{backgroundColor: "blue", color: "white"}}>Edit Profile</button>
         {children}
-        </>
+        </div>
     )
 }
 
