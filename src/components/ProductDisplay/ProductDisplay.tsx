abfd2a4ada84a8ed02d77403ffc34955cc988dd8
scriptMang <aperalta686@gmail.com>
2026-03-04 02:49:26 -0500

    Change fragment into div w/ id product-card for product-display component

--- src/components/ProductDisplay/ProductDisplay.tsx
+++ src/components/ProductDisplay/ProductDisplay.tsx
@@ -3,13 +3,13 @@ import type { ProductDisplayProps } from '../../types'
 function ProductDisplay({product, showDescription, showStockStatus, onAddToCart}: ProductDisplayProps) {
  onAddToCart ? onAddToCart("Item added to cart") : console.log("no funct");
     return (
-        <>
+        <div id="productCard">
          <div id="productName">{product.name}</div>
          <div id="productPrice">${product.price}</div>
          { showDescription ? <p>{product.description}</p> : showDescription }
          { product.imageUrl ? <img src={product.imageUrl} id="productImg" style={{height: "300px", width: "300px"}} /> : product.imageUrl}
          { showStockStatus ?  <p id="itemInStock">Product is in stock</p> : showStockStatus}
-        </>
+        </div>
     )
 }
 
